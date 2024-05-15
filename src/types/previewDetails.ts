60dc7cbe5e5ce3698a3764541b02ca72d6f0c338
toastycoder0 <re-jos2002@hotmail.com>
2024-05-15 16:20:42 -0500

    feat: added preview details interface

---
+++ src/types/previewDetails.ts
@@ -0,0 +1,11 @@
+export interface PreviewDetails {
+  illustration: string
+  date: string
+  title: string
+  description: string
+  type: string
+  user: {
+    title: string
+    avatar: string
+  }
+}
